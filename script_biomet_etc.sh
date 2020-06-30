4dd097858de9521f5bb4d3d9aeb98301633eb200
seb inra <sebastien.lafont@inra.fr>
2020-06-30 17:07:39 +0200

    last version by christophe with bash driver

---
+++ script_biomet_etc.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+
+python3.4 /DATA/BILOS/git/script/cp/script_etc_file.py  '/DATA/BILOS/RAW/ICOS/ANNEEENCOURS/icos/rayonnement/CR3000_rayonnement_ICOS_RAY_20S.dat' '/DATA/BILOS/RAW/ICOS/ANNEEENCOURS/entete/FR-Bil_BMHEADER_201711010000_L02_F01.csv' '/DATA/BILOS/RAW/ICOS/ANNEEENCOURS/tmp2/' '/DATA/BILOS/RAW/ICOS/ANNEEENCOURS/icos/FICHIERCP2/' 'toto'
